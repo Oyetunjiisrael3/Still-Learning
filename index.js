2e3b5e735beee8bc5e765c34ded7bd8b09bffb3d
Oyetunji Israel <oyetunjiisrael3@gmail.com>
2022-05-16 09:28:42 +0100

    basic index file created with a console message - tring to be perfect

---
+++ index.js
@@ -0,0 +1,8 @@
+echo "# Still-Learning" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Oyetunjiisrael3/Still-Learning.git
+git push -u origin main
+console .assert
